30ce301bc9c3a032bd8236641aa7e8fe95d481f1
RealAnomalyGames <146598908+RealAnomalyGames@users.noreply.github.com>
2026-08-10 19:01:27 +0000

    Fix TypeScript element nullability

--- src/main.ts
+++ src/main.ts
@@ -6,33 +6,23 @@ import type { Note } from "./types/Note";
 
 const newNoteButton = document.querySelector<HTMLButtonElement>(
   "#new-note-button"
-);
+)!;
 
 const searchInput = document.querySelector<HTMLInputElement>(
   "#search-input"
-);
+)!;
 
 const noteList = document.querySelector<HTMLElement>(
   "#note-list"
-);
+)!;
 
 const noteTitle = document.querySelector<HTMLInputElement>(
   "#note-title"
-);
+)!;
 
 const noteContent = document.querySelector<HTMLTextAreaElement>(
   "#note-content"
-);
-
-if (
-  !newNoteButton ||
-  !searchInput ||
-  !noteList ||
-  !noteTitle ||
-  !noteContent
-) {
-  throw new Error("Neptune Notes: Required UI elements were not found.");
-}
+)!;
 
 let notes: Note[] = [];
 let activeNoteId: string | null = null;
